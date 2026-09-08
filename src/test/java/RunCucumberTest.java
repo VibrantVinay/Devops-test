e9d8063fff5de253ba713310e66ccf68ceb6e0fa
VinayasCoder <84186178+VibrantVinay@users.noreply.github.com>
2026-09-08 18:48:03 +0530

    Update and rename RunCucumberTest.java to src/test/java/RunCucumberTest.java

---
+++ src/test/java/RunCucumberTest.java
@@ -0,0 +1,10 @@
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = "com.project.stepdefinitions",
+    plugin = {"pretty", "html:target/cucumber-reports.html"}
+)
+public class RunCucumberTest extends AbstractTestNGCucumberTests {
+}
